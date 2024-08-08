a33ef332cf960760ecb9e4bd4be12eded1cfab1d
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-08-08 19:57:15 -0400

    decapitalized "a:Actor" and "m:Movie" to "a:actor" and "m:movie"

--- src/main/java/ca/yorku/eecs/DB/DBNew.java
+++ src/main/java/ca/yorku/eecs/DB/DBNew.java
@@ -10,20 +10,20 @@ public class DBNew {
 	
 	public void addRatingToAllMovies(double defaultRating) {
 		try(Session session = DBUtil.getSession()){
-			session.run("MATCH (m:Movie) SET m.rating = $rating", Map.of("rating", String.format("%.2f", defaultRating)));
+			session.run("MATCH (m:movie) SET m.rating = $rating", Map.of("rating", String.format("%.2f", defaultRating)));
 		}
 	}
 	
 	public void updateMovieRating(String movieTitle, double newRating) {
 		try(Session session = DBUtil.getSession()){
-			session.run("MATCH (m:Movie {title: $title}) SET m.rating = $rating", Map.of("title", movieTitle, "rating", String.format("rating", String.format("%.1f", newRating))));
+			session.run("MATCH (m:movie {title: $title}) SET m.rating = $rating", Map.of("title", movieTitle, "rating", String.format("rating", String.format("%.1f", newRating))));
 		}
 	}
 	
 	public List<String> getMoviesWithRating(double minRating) {
 		List<String> movies = new ArrayList<>();
 		try(Session session = DBUtil.getSession()){
-			StatementResult result = session.run("MATCH (m:Movie) WHERE m.rating >= $rating RETURN m.title AS title", Map.of("rating", String.format("%.1f", minRating)));
+			StatementResult result = session.run("MATCH (m:movie) WHERE m.rating >= $rating RETURN m.title AS title", Map.of("rating", String.format("%.1f", minRating)));
 			while(result.hasNext()) {
 				Record record = result.next();
 				movies.add(record.get("title").asString());
@@ -35,7 +35,7 @@ public class DBNew {
 	public List<String> getMoviesByReleaseYear(int year){
 		List<String> movies = new ArrayList<>();
 		try(Session session = DBUtil.getSession()){
-			StatementResult result = session.run("MATCH (m:Movie) WHERE m.release = $release RETURN m.title AS title", Map.of("release", year));
+			StatementResult result = session.run("MATCH (m:movie) WHERE m.release = $release RETURN m.title AS title", Map.of("release", year));
 			while(result.hasNext()) {
 				Record record = result.next();
 				movies.add(record.get("title").asString());
@@ -46,7 +46,7 @@ public class DBNew {
 	}
 	public void addAwards(String actor, List<String> awards) {
 		try(Session session = DBUtil.getSession()){
-			session.run("MATCH (a:Actor {name: $name}) SET a.awards = $awards",
+			session.run("MATCH (a:actor {name: $name}) SET a.awards = $awards",
 	                Map.of("name", actor, "awards", awards));	
 		}
 	}
@@ -54,7 +54,7 @@ public class DBNew {
 	public List<String> getActorsByAward(String award){
 		List<String> actors = new ArrayList<>();
 		try(Session session = DBUtil.getSession()){
-			StatementResult result = session.run("MATCH (a:Actor) WHERE $award IN a.awards RETURN a.name AS name", Map.of("award", award));
+			StatementResult result = session.run("MATCH (a:actor) WHERE $award IN a.awards RETURN a.name AS name", Map.of("award", award));
 			while(result.hasNext()) {
 				Record record = result.next();
 				actors.add(record.get("name").asString());
