79b16eb42b613d4f7a29cd5d6c299b5d3d3cdc01
sharpjd <59419827+sharpjd@users.noreply.github.com>
2024-08-09 00:00:00 -0400

    handle missing info correctly

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -46,6 +46,7 @@ public class App //starter code
         }
         
         server.createContext("/api/v1/addActor", new AddActorHttpHandler(db));
+        server.createContext("/api/v1/addMovie", new AddMovieHttpHandler(db));
     }
     
 }
@@ -121,22 +122,127 @@ class AddActorHttpHandler implements HttpHandler {
         
         try {
 			jsonObject = new JSONObject(json);
-			actorName = jsonObject.getString("name");
-			actorId = jsonObject.getString("actorId");
+			actorName = jsonObject.optString("name");
+			actorId = jsonObject.optString("actorId");
+			
+			if(actorName == null || actorName.isEmpty()) {
+	        	valid = false;
+	        	message.append("Validation failed: actor name is empty or not found\n");
+	        }
+	        
+	        if(actorId == null || actorId.isEmpty()) {
+	        	valid = false;
+	        	message.append("Validation failed: actorId is empty or not found\n");
+	        }
+			
 		} catch (JSONException e) {
 			message.append("Validation failed: JSON syntax error: " + e.getMessage());
 			return new JSONValidationData(false, message.toString());
 		}
         
-        if(actorName == null || actorName.isEmpty()) {
-        	valid = false;
-        	message.append("Validation failed: actor name is empty\n");
-        }
+        return new JSONValidationData(valid, message.toString());
+		
+	}
+	
+	private void sendResponseAndClose(HttpExchange exchange, int code, String response) throws IOException {
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+	}
+	
+}
+
+class AddMovieHttpHandler implements HttpHandler {
+
+	private DBNew db;
+	
+	public AddMovieHttpHandler(DBNew db) {
+		this.db = db;
+	}
+	
+	@Override
+	public void handle(HttpExchange exchange) {
+		
+		try {
+			System.out.println("Got an AddMovie request!");
+			
+			if ("PUT".equals(exchange.getRequestMethod())) {
+				
+	            String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+	            System.out.println("Request body:" + requestBody);
+	            
+	            JSONValidationData validation = validateJSON(requestBody);
+	            
+	            if(validation.valid) {
+	            	
+	            	try {
+	        			JSONObject jsonObject = new JSONObject(requestBody);
+	        			String movieName = jsonObject.getString("name");
+	        			String movieId = jsonObject.getString("movieId");
+	        			
+	        			String movieRelease = jsonObject.optString("release"); //can be empty
+	        			
+	        			db.addMovie(movieId, movieName, movieRelease);
+	        			
+	        		} catch (JSONException e) {
+	        			e.printStackTrace();
+	        		}
+	            	
+	            	//respond with success message
+	                String response = "PUT request successful. Data: " + requestBody;
+	                sendResponseAndClose(exchange, 200, response);
+	            } else {
+	            	
+	            	//respond with fail message
+	            	String response = "PUT request failed with the following message:\n"
+	            			+ validation.message
+	            			+ " Data: " + requestBody;
+	            	sendResponseAndClose(exchange, 400, response);
+	            }
+	            
+	            
+	        } else {
+	            sendResponseAndClose(exchange, 405, "Only PUT is supported");
+	        }
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Handle AddMovie finished");
+		
+	}
+	
+	public JSONValidationData validateJSON(String json) {
+		
+		StringBuilder message = new StringBuilder();
+		boolean valid = true;
+		
+		JSONObject jsonObject;
+        String movieName = null;
+        String movieId = null;
+        //skip checking for release date because that's optional
         
-        if(actorId == null || actorName.isEmpty()) {
-        	valid = false;
-        	message.append("Validation failed: actorId is empty\n");
-        }
+        try {
+			jsonObject = new JSONObject(json);
+			movieName = jsonObject.optString("name");
+			movieId = jsonObject.optString("movieId");
+			
+			if(movieName == null || movieName.isEmpty()) {
+	        	valid = false;
+	        	message.append("Validation failed: movie name is empty or not found\n");
+	        }
+	        
+	        if(movieId == null || movieId.isEmpty()) {
+	        	valid = false;
+	        	message.append("Validation failed: movieId is empty or not found\n");
+	        }
+			
+		} catch (JSONException e) {
+			message.append("Validation failed: JSON syntax error: " + e.getMessage());
+			return new JSONValidationData(false, message.toString());
+		}
         
         return new JSONValidationData(valid, message.toString());
 		
